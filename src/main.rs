a7d8f50abbf03c840d5f7c0c22b7d069ced52257
Thomas Forbes <tom.forbes@gitguardian.com>
2025-01-24 14:56:18 +0000

    WIP

--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,34 @@ use gix_protocol::fetch::negotiate::{Action, Error, Round};
 use gix_protocol::fetch::{handshake, Arguments, Context, Negotiate, Response};
 use gix_protocol::transport::connect::Options;
 use std::sync::atomic::AtomicBool;
+use clap::builder::Str;
+use clap::Parser;
 use gix_hash::Kind;
 
-struct Negotiator {}
+#[derive(Debug, clap::Parser)]
+struct Args {
+    repo: String,
+    blobs: Vec<String>,
+}
+
+struct Negotiator {
+    blobs: Vec<String>
+}
 
 impl Negotiate for Negotiator {
     fn mark_complete_and_common_ref(&mut self) -> Result<Action, Error> {
         Ok(Action::MustNegotiate {
             remote_ref_target_known: vec![],
         })
-        // todo!("mark_complete_and_common_ref")
     }
 
     fn add_wants(&mut self, arguments: &mut Arguments, remote_ref_target_known: &[bool]) -> bool {
-        let blob_id = const_hex::decode("a46bd72d1f660ee3f1d9482bb52fdd8b15683c4a").unwrap();
-        arguments.want(gix_hash::oid::try_from_bytes(&blob_id).unwrap());
+        arguments.filter("blob:none");
+        for oid in &self.blobs {
+            let blob_id = const_hex::decode(oid).unwrap();
+            arguments.want(gix_hash::oid::try_from_bytes(&blob_id).unwrap());
+        }
+
         true
     }
 
@@ -39,11 +52,13 @@ impl Negotiate for Negotiator {
 }
 
 fn main() {
+    let args = Args::parse();
+    
     let progress = prodash::tree::Root::new();
     // let progress = prod
     let options = Options::default();
     let mut transport =
-        gix_protocol::transport::client::connect("git@github.com:orf/gping.git", options).unwrap();
+        gix_protocol::transport::client::connect(args.repo, options).unwrap();
 
     let mut handshake = handshake(
         &mut transport,
@@ -54,9 +69,11 @@ fn main() {
     .unwrap();
 
 
-    println!("Connected! {handshake:#?}");
+    // println!("Connected! {handshake:#?}");
 
-    let mut negotiator = Negotiator {};
+    let mut negotiator = Negotiator {
+        blobs: args.blobs,
+    };
     let mut interrupt = AtomicBool::default();
 
     let context = Context {
@@ -72,8 +89,6 @@ fn main() {
             let mut file = std::fs::File::create("output.pack").unwrap();
             std::io::copy(reader, &mut file)?;
             Ok::<_, std::io::Error>(true)
-            // todo!("Progress!");
-            // Ok::<_, std::io::Error>(true)
         },
         &mut progress.add_child("fetch"),
         &mut interrupt,
